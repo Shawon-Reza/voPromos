e67b0414ef746315280b80e421da849452f68321
Shawon Reza <shawon.reza.sr81@gmail.com>
2025-11-15 10:09:46 +0600

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import router from './routes/route.jsx'
+import { RouterProvider } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ToastContainer />
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
