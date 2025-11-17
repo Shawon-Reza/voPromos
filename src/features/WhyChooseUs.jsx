c2a54a04b98a86523438e335782ae6023516cd9b
Shawon Reza <shawon.reza.sr81@gmail.com>
2025-11-17 15:15:34 +0600

    icon full circl;e prblm solve

--- src/features/WhyChooseUs.jsx
+++ src/features/WhyChooseUs.jsx
@@ -32,9 +32,9 @@ const WhyChooseUs = () => {
             <h2 className="mt-2 text-[#004260] text-2xl md:text-3xl xl:text-4xl 2xl:text-5xl font-extrabold">Why Choose VoPromos</h2>
 
             <ul className="mt-6 space-y-4 xl:text-xl 2xl:text-2xl ">
-              <li className="flex gap-3">
-                <span className="mt-1 inline-flex items-center justify-center w-7 h-7 rounded-full bg-linear-to-r from-[#152035] to-[#004E7B] text-white">
-                  <GiCheckMark />
+              <li className="flex gap-3 items-start">
+                <span className="shrink-0 inline-flex items-center justify-center w-7 h-7 rounded-full bg-linear-to-r from-[#152035] to-[#004E7B] text-white">
+                  <GiCheckMark className="w-3.5 h-3.5" />
                 </span>
                 <div>
                   <strong className="text-slate-800">End-To-End Services:</strong>
@@ -42,9 +42,9 @@ const WhyChooseUs = () => {
                 </div>
               </li>
 
-              <li className="flex gap-3">
-                <span className="mt-1 inline-flex items-center justify-center w-7 h-7 rounded-full bg-linear-to-r from-[#152035] to-[#004E7B] text-white">
-                  <GiCheckMark />
+              <li className="flex gap-3 items-start">
+                <span className="shrink-0 inline-flex items-center justify-center w-7 h-7 rounded-full bg-linear-to-r from-[#152035] to-[#004E7B] text-white">
+                  <GiCheckMark className="w-3.5 h-3.5" />
                 </span>
                 <div>
                   <strong className="text-slate-800">Global Network:</strong>
@@ -52,9 +52,9 @@ const WhyChooseUs = () => {
                 </div>
               </li>
 
-              <li className="flex gap-3">
-                <span className="mt-1 inline-flex items-center justify-center w-7 h-7 rounded-full bg-linear-to-r from-[#152035] to-[#004E7B] text-white">
-                  <GiCheckMark />
+              <li className="flex gap-3 items-start">
+                <span className="shrink-0 inline-flex items-center justify-center w-7 h-7 rounded-full bg-linear-to-r from-[#152035] to-[#004E7B] text-white">
+                  <GiCheckMark className="w-3.5 h-3.5" />
                 </span>
                 <div>
                   <strong className="text-slate-800">Cost Optimization:</strong>
@@ -62,9 +62,9 @@ const WhyChooseUs = () => {
                 </div>
               </li>
 
-              <li className="flex gap-3">
-                <span className="mt-1 inline-flex items-center justify-center w-7 h-7 rounded-full bg-linear-to-r from-[#152035] to-[#004E7B] text-white">
-                  <GiCheckMark />
+              <li className="flex gap-3 items-start">
+                <span className="shrink-0 inline-flex items-center justify-center w-7 h-7 rounded-full bg-linear-to-r from-[#152035] to-[#004E7B] text-white">
+                  <GiCheckMark className="w-3.5 h-3.5" />
                 </span>
                 <div>
                   <strong className="text-slate-800">Risk-Free:</strong>
@@ -86,31 +86,31 @@ const WhyChooseUs = () => {
                <h4 className="text-slate-900 text-lg xl:text-xl 2xl:text-2xl font-semibold">Industries We Serve</h4>
 
               <li className="flex gap-3 items-center">
-                <span className="mt-1 inline-flex items-center justify-center w-7 h-7 rounded-full bg-linear-to-r from-[#152035] to-[#004E7B] text-white">
-                  <GiCheckMark />
+                <span className="inline-flex items-center justify-center w-7 h-7 rounded-full bg-gradient-to-r from-[#152035] to-[#004E7B] text-white">
+                  <GiCheckMark className="w-3.5 h-3.5" />
                 </span>
                 <span className="text-[#004260] font-semibold  xl:text-xl 2xl:text-2xl">Retail & E-Commerce</span>
               </li>
               <li className="flex gap-3 items-start">
-                <span className="mt-1 inline-flex items-center justify-center w-7 h-7 rounded-full bg-linear-to-r from-[#152035] to-[#004E7B] text-white">
-                  <GiCheckMark />
+                <span className="inline-flex items-center justify-center w-7 h-7 rounded-full bg-gradient-to-r from-[#152035] to-[#004E7B] text-white">
+                  <GiCheckMark className="w-3.5 h-3.5" />
                 </span>
                 <span className="text-[#004260] font-semibold  xl:text-xl 2xl:text-2xl">Promotional Products & Marketing</span>
               </li>
               <li className="flex gap-3 items-start">
-                <span className="mt-1 inline-flex items-center justify-center w-7 h-7 rounded-full bg-linear-to-r from-[#152035] to-[#004E7B] text-white">
+                <span className="inline-flex items-center justify-center w-7 h-7 rounded-full bg-gradient-to-r from-[#152035] to-[#004E7B] text-white">
                   <GiCheckMark />
                 </span>
                 <span className="text-[#004260] font-semibold  xl:text-xl 2xl:text-2xl">Manufacturing & Packaging</span>
               </li>
               <li className="flex gap-3 items-start">
-                <span className="mt-1 inline-flex items-center justify-center w-7 h-7 rounded-full bg-linear-to-r from-[#152035] to-[#004E7B] text-white">
+                <span className="inline-flex items-center justify-center w-7 h-7 rounded-full bg-gradient-to-r from-[#152035] to-[#004E7B] text-white">
                   <GiCheckMark />
                 </span>
                 <span className="text-[#004260] font-semibold  xl:text-xl 2xl:text-2xl">Food & Beverage Supply</span>
               </li>
               <li className="flex gap-3 items-start">
-                <span className="mt-1 inline-flex items-center justify-center w-7 h-7 rounded-full bg-linear-to-r from-[#152035] to-[#004E7B] text-white">
+                <span className="inline-flex items-center justify-center w-7 h-7 rounded-full bg-gradient-to-r from-[#152035] to-[#004E7B] text-white">
                   <GiCheckMark />
                 </span>
                 <span className="text-[#004260] font-semibold  xl:text-xl 2xl:text-2xl">Startups & Private Label Brands</span>
